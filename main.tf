601b0962af11ee022203090ec0bbac3a007eb138
munnep <patrick.munne@hashicorp.com>
2024-07-02 14:08:49 +0200

    test

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 module "test153119" {
   source  = "tfe66.aws.munnep.com/test/test153119/azurerm"
-  version = "1.1.5"
+  version = "1.1.6"
 #    providers = {
 #    azurerm = azurerm.private-provider
 #  }
