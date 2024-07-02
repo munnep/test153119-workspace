7f58b8dfacf4b1a4dee648598312be3bcc779918
munnep <patrick.munne@hashicorp.com>
2024-07-02 12:21:25 +0200

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-module "test153119" {
-  source  = "tfe66.aws.munnep.com/test/test153119/azurerm"
-  version = "1.1.0"
-}
-
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.110.0"
+      source = "tfe66.aws.munnep.com/test/azurerm"
+      version = "3.107.0"
     }
   }
 }
@@ -16,6 +11,10 @@ provider "azurerm" {
   features {}
 }
 
+module "test153119" {
+  source  = "tfe66.aws.munnep.com/test/test153119/azurerm"
+  version = "1.1.0"
+}
 
 output "test" {
     value = "working"
